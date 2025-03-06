5d607da77ac602da5e996e2ee92cea7c49b6b91d
Max Herbert <w0494420@campus.nscc.ca>
2025-03-06 16:31:39 -0400

    added test

--- LinkedLists/main.c
+++ LinkedLists/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include "linked_list.h"
 
 int main(void)
 {
     printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    LinkedList * my_linked_list = list_create();
+    if (my_linked_list == NULL) {
+        printf("Error creating list!\n");
+    } else {
+        printf("List created successfully!\n");
+    }
     return 0;
 }
